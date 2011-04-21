2af07fccb8dd6f4eda1374b47cd00d188d0045b4
Daisuke (yet another) Maki <maki.daisuke@gmail.com>
2011-04-21 17:54:14 -0500

    initial commit

---
+++ lib/Apache2/Authen/Redmine.pm
@@ -0,0 +1,32 @@
+package Apache2::Authen::Redmine;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Apache2::Authen::Redmine -
+
+=head1 SYNOPSIS
+
+  use Apache2::Authen::Redmine;
+
+=head1 DESCRIPTION
+
+Apache2::Authen::Redmine is
+
+=head1 AUTHOR
+
+Daisuke (yet another) Maki E<lt>yanother {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
